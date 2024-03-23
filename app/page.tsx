1dc6e47b248e0a1fe032665bf3cb8a08aa2d9ae2
Tarun <tarunpahade6969@gmail.com>
2024-03-23 22:08:37 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Particles from "./components/particles";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
+  { name: "Skills", href: "/skills" },
   { name: "Contact", href: "/contact" },
 ];
 
